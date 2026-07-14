476700f5cfc2d31cc7bb464b9650254faada1862
ygt-design <103063122+ygt-design@users.noreply.github.com>
2026-07-14 10:39:26 -0400

    Add Pricing page and route

---
+++ src/pages/Pricing/Pricing.tsx
@@ -0,0 +1,13 @@
+import styles from './Pricing.module.css';
+
+const Pricing = () => {
+  return (
+    <div className={styles.pricingPage}>
+      <div className={styles.container}>
+        <h1>Pricing</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
